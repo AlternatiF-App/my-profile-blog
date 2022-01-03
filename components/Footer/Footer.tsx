7cc65cf1d31b4252b3a6ab3c630545872f14d138
Fanani <fananiapps@gmail.com>
2022-01-03 09:09:47 +0700

    Fix Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,14 +4,24 @@ const Footer = () => {
   return (
         <div className="lg:flex w-full 2xl:w-10/12 mx-auto px-8 py-14">
             <div className="w-full lg:w-1/2">
-                <span className="block text-6xl text-primary">LOGO</span>
+                <span className="block text-center lg:text-left text-6xl text-primary">LOGO</span>
             </div>
-            <div className="w-full lg:w-1/2 pt-8 lg:pt-0">
-                <h2 className="mb-4 font-medium text-lg">Menu</h2>
-                <div className="space-y-2">
-                    <ItemFooter name={'CV'}/>
-                    <ItemFooter name={'Profile'}/>
-                    <ItemFooter name={'Blogs'}/>
+            <div className="w-full lg:w-1/2 pt-8 lg:pt-0 flex">
+                <div className="w-1/2">
+                    <h2 className="mb-4 font-medium text-lg">Menu</h2>
+                    <div className="space-y-2">
+                        <ItemFooter name={'CV'}/>
+                        <ItemFooter name={'Profile'}/>
+                        <ItemFooter name={'Blogs'}/>
+                    </div>
+                </div>
+                <div className="w-1/2">
+                    <h2 className="mb-4 font-medium text-lg">Menu</h2>
+                    <div className="space-y-2">
+                        <ItemFooter name={'CV'}/>
+                        <ItemFooter name={'Profile'}/>
+                        <ItemFooter name={'Blogs'}/>
+                    </div>
                 </div>
             </div>
         </div>
