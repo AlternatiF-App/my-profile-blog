5fb16706fcd89c53aae94c68a10c2cb908d8bb62
Fanani <fananiapps@gmail.com>
2022-01-17 15:27:20 +0700

    Fix Portofolio Gap

--- pages/portofolio.tsx
+++ pages/portofolio.tsx
@@ -10,7 +10,7 @@ const portofolio = () => {
                     <div className="w-full 2xl:w-11/12 lg:mx-auto px-4 py-10 lg:px-8 lg:py-36 hide-scroll-bar">
                         <h1 className="text-xl lg:text-3xl font-medium tracking-wider text-white">Portofolio</h1>
                         <div className="h-1 w-10 bg-secondary rounded-full"/>
-                        <div className="lg:grid lg:grid-cols-4 gap-8 pt-10">
+                        <div className="lg:grid lg:grid-cols-4 gap-8 space-y-10 lg:space-y-0 pt-10">
                             <ItemPortofolio image={'porto-1.png'}/>
                             <ItemPortofolio image={'porto-2.png'}/>
                             <ItemPortofolio image={'porto-3.png'}/>
